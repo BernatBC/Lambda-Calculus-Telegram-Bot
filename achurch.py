b15cd7f457d6760e1b7466b307bddcb666603914
BernatBC <bernat.borrasc@gmail.com>
2023-05-15 17:33:31 +0200

    Finish Tasca 2: el visitador

--- achurch.py
+++ achurch.py
@@ -1,6 +1,71 @@
+from __future__ import annotations
+from dataclasses import dataclass
 from antlr4 import *
 from lcLexer import lcLexer
 from lcParser import lcParser
+from lcVisitor import lcVisitor
+
+
+class Buit:
+    pass
+
+@dataclass
+class Node:
+    esq: Arbre
+    dre: Arbre
+
+@dataclass
+class Abstraccio(Node):
+    esq: Arbre
+    dre: Arbre
+
+@dataclass
+class Aplicacio(Node):
+    esq: Arbre
+    dre: Arbre
+
+@dataclass
+class Variable:
+    variable: chr
+
+Arbre = Node | Variable
+
+def to_string(a: Arbre) -> str:
+    match a:
+        case Abstraccio(esquerra, dreta):
+            return '(λ' + str(to_string(esquerra)) + '.' + to_string(dreta) + ')'
+        case Aplicacio(esquerra, dreta):
+            return '(' + to_string(esquerra) + to_string(dreta) + ')'
+        case Variable(var):
+            return var
+
+
+class TreeVisitor(lcVisitor):
+
+    def visitRoot(self, ctx:lcParser.RootContext):
+        return self.visitChildren(ctx)
+
+    def visitParentesis(self, ctx:lcParser.ParentesisContext):
+        [_, terme, _] = list(ctx.getChildren())
+        return self.visit(terme)
+
+    def visitLletra(self, ctx:lcParser.LletraContext):
+        lletra = str(ctx.getChild(0))
+        return Variable(lletra)
+
+    def visitAbstraccio(self, ctx:lcParser.AbstraccioContext):
+        terme = ctx.getChild(ctx.getChildCount() - 1)
+        lletra = ctx.getChild(ctx.getChildCount() - 3)
+        n = Abstraccio(Variable(lletra), self.visit(terme))
+        # traducció abstracció de múltiples paràmetres
+        for i in range(ctx.getChildCount() - 4, 0, -1):
+            n = Abstraccio(Variable(ctx.getChild(i)), n)
+        return n
+
+    def visitAplicacio(self, ctx:lcParser.AplicacioContext):
+        [terme1, terme2] = list(ctx.getChildren())
+        return Aplicacio(self.visit(terme1), self.visit(terme2))
+
 
 input_stream = InputStream(input('? '))
 lexer = lcLexer(input_stream)
@@ -8,5 +73,11 @@ token_stream = CommonTokenStream(lexer)
 parser = lcParser(token_stream)
 tree = parser.root()
 
-print(parser.getNumberOfSyntaxErrors(), 'errors de sintaxi.')
-print(tree.toStringTree(recog=parser))
+if parser.getNumberOfSyntaxErrors() == 0:
+    visitor = TreeVisitor()
+    a = visitor.visit(tree)
+    print('Arbre:')
+    print(to_string(a))
+else:
+    print(parser.getNumberOfSyntaxErrors(), 'errors de sintaxi.')
+    print(tree.toStringTree(recog=parser))
