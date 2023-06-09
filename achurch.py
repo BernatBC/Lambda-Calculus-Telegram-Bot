a24d3e4a3bc1a9987109017e2b2d294efcf7fa6c
BernatBC <bernat.borrasc@gmail.com>
2023-06-09 19:01:50 +0200

    Document code

--- achurch.py
+++ achurch.py
@@ -4,52 +4,180 @@ from antlr4 import *
 from lcLexer import lcLexer
 from lcParser import lcParser
 from lcVisitor import lcVisitor
-
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, filters, MessageHandler
 import math
 import pydot
 import sys
 
+# Definició de l'arbre d'una expressió
 @dataclass
 class Abstraccio:
+    """Node abstracció
+    """
+
     var: Variable
     expr: Arbre
 
 @dataclass
 class Aplicacio:
+    """Node aplicació
+    """
+
     esq: Arbre
     dre: Arbre
 
 @dataclass
 class Variable:
+    """Node variable
+    """
+
     var: str
 
+# Definició d'arbre com a subclasse dels nodes definits
 Arbre = Abstraccio | Aplicacio | Variable
 
 # Diccionari que emmagatzema els arbres de cada macro. Clau: Nom de la macro, Valor: Arbre de la macro
 macros = {}
 
+# Llista de passos fets, utilitzat per imprimir-los per telegram
 steps = []
 
-# Retorna l'arbre en format de string
+
+class TreeVisitor(lcVisitor):
+    """Visitador de la gramàtica
+
+    Args:
+        lcVisitor (lcVisitor): lcVisitador de la gramàtica
+    """
+
+    def visitAssignacio(self, ctx:lcParser.AssignacioContext):
+        """Visitador de l'assignació d'una macro
+
+        Args:
+            ctx (lcParser.AssignacioContext): Node de la gramàtica
+
+        Returns:
+            Bool: True -> s'ha produït l'assignació
+        """
+
+        variable = str(ctx.getChild(0))
+        terme = ctx.getChild(2)
+        macros[variable] = self.visit(terme)
+        return True
+
+    def visitParentesis(self, ctx:lcParser.ParentesisContext):
+        """Visitador d'un terme entre parèntesis
+
+        Args:
+            ctx (lcParser.ParentesisContext): Node de la gramàtica
+
+        Returns:
+            Arbre: Arbre resultant del terme introduït
+        """
+
+        terme = ctx.getChild(1)
+        return self.visit(terme)
+
+    def visitLletra(self, ctx:lcParser.LletraContext):
+        """Visitador d'una variable
+
+        Args:
+            ctx (lcParser.ParentesisContext): Node de la gramàtica
+
+        Returns:
+            Variable: Variable del node
+        """
+                
+        lletra = str(ctx.getChild(0))
+        return Variable(lletra)
+
+    def visitVariable(self, ctx:lcParser.VariableContext):
+        """Visitador d'una variable de macro
+
+        Args:
+            ctx (lcParser.ParentesisContext): Node de la gramàtica
+
+        Returns:
+            Arbre: Arbre corresponent al nom de la macro
+        """
+
+        variable = str(ctx.getChild(0))
+        return macros[variable]
+
+    def visitAbstraccio(self, ctx:lcParser.AbstraccioContext):
+        """Visitador d'una abstracció.
+
+        Args:
+            ctx (lcParser.ParentesisContext): Node de la gramàtica
+
+        Returns:
+            Abstraccio: Node abstraccio resultant
+        """
+
+        terme = ctx.getChild(ctx.getChildCount() - 1)
+        lletra = ctx.getChild(ctx.getChildCount() - 3)
+        n = Abstraccio(Variable(lletra), self.visit(terme))
+        # traducció abstracció de múltiples paràmetres
+        for i in range(ctx.getChildCount() - 4, 0, -1): n = Abstraccio(Variable(ctx.getChild(i)), n)
+        return n
+
+    def visitAplicacio(self, ctx:lcParser.AplicacioContext):
+        """Visitador d'una aplicació
+
+        Args:
+            ctx (lcParser.ParentesisContext): Node de la gramàtica
+
+        Returns:
+            Abstraccio: Node aplicació resultant
+        """
+
+        [terme1, terme2] = list(ctx.getChildren())
+        x = str(terme2.getChild(0))
+        # En el cas que es detecti una macro en notació infixa, convertir-la en notació prefixa
+        if (len(x) == 1 and not x.isalpha() and x != '(' and x != '\u005C' and x != 'λ'): 
+            return Aplicacio(self.visit(terme2), self.visit(terme1))
+        return Aplicacio(self.visit(terme1), self.visit(terme2))
+
+
 def to_string(arbre: Arbre) -> str:
+    """Retorna l'arbre en format de string
+
+    Args:
+        arbre (Arbre): Arbre a convertir
+
+    Returns:
+        str: Cadena de caràcters que representa l'arbre
+    """
     match arbre:
         case Abstraccio(variable, expressio):
             return '(λ' + str(to_string(variable)) + '.' + str(to_string(expressio)) + ')'
         case Aplicacio(esquerra, dreta):
             return '(' + str(to_string(esquerra)) + str(to_string(dreta)) + ')'
         case Variable(var):
-            return var            
+            return var
 
-# Retrona l'arbre després d'aplicar les alpha-conversions
+
 def alpha(arbre: Arbre):
+    """Aplica alpha-conversions en un arbre
+
+    Args:
+        arbre (Arbre): Arbre a alpha-convertir
+
+    Returns:
+        Arbre: Retrona l'arbre després d'aplicar les alpha-conversions
+    """
 
     # Conjunt que emmagatzema els noms de totes les variables que apareixen a l'arbre abans d'aplicar l'alpha-conversió
     utilitzades = set()
 
-    # Mètode que afageix al conjunt "utilitzades" les variables que apareixen a l'arbre abans d'aplicar l'alpha-conversió
     def variables_utilitzades(a: Arbre):
+        """Mètode que afageix al conjunt "utilitzades" les variables que apareixen a l'arbre abans d'aplicar l'alpha-conversió
+
+        Args:
+            a (Arbre): Arbre a visitar
+        """
+
         match a:
             case Abstraccio(variable, expression):
                 variables_utilitzades(expression)
@@ -60,8 +188,17 @@ def alpha(arbre: Arbre):
             case Variable(var):
                 utilitzades.add(var)
 
-    # Mètode que fa les alpha-conversions
     def alpha_conversio(a: Arbre, parametres):
+        """Mètode que fa les alpha-conversions
+
+        Args:
+            a (Arbre): Arbre a alpha-convertir
+            parametres (_type_): Llista dels paràmetres de les funcions lambda tals que fa afecte a l'arbre a
+
+        Returns:
+            Arbre: Arbre alpha-convertit
+        """
+
         match a:
             case Abstraccio(variable, expression):
                 v = str(variable.var)
@@ -114,8 +251,18 @@ def alpha(arbre: Arbre):
                     lliures.add(var)
                 return (a, lliures, lligades)
     
-    # Substitueix els noms de les variables d'antiga a nove del arbre
     def conversio(a: arbre, antiga, nova):
+        """Substitueix els noms de les variables d'antiga a nove del arbre
+
+        Args:
+            a (arbre): Arbre a convertir
+            antiga (str): nom de la variable per substituir
+            nova (str): nom de la variable substituda
+
+        Returns:
+            Arbre: Arbre convertit
+        """
+
         match a:
             case Abstraccio(variable, expression):
                 exp_convertida = conversio(expression, antiga, nova)
@@ -131,8 +278,13 @@ def alpha(arbre: Arbre):
                     return Variable(nova)
                 return a
 
-    # Retorna un nom de variable sense utilitzar, i el marca com a utilitzada
     def assignar_variable():
+        """Retorna un nom de variable sense utilitzar, i el marca com a utilitzada
+
+        Returns:
+            str: nom de la variable
+        """
+
         # Si ens quedem sense variables, afegim cometes al final de la variable
         n_cometes = 0
         while True:
@@ -147,14 +299,30 @@ def alpha(arbre: Arbre):
     variables_utilitzades(arbre)
     return alpha_conversio(arbre, set())[0]
 
-# Retrona l'arbre després d'aplicar una alpha-conversió
+
 def beta(a: Arbre):
+    """Mètode que aplica beta-reduccions
+
+    Args:
+        a (Arbre): Arbre a beta-reduir
+
+    Returns:
+        Arbre: Arbre beta-reduit
+    """
 
     # Compta el nombre de beta-reduccions fetes
     n_reduccions = 0
 
-    # Mètode que aplica una beta-reducció (o cap si no n'hi ha cap de possible)
     def beta_reduccio(arbre: Arbre) -> Arbre:
+        """Mètode que aplica una beta-reducció (o cap si no n'hi ha cap de possible)
+
+        Args:
+            arbre (Arbre): Arbre a beta-reduit
+
+        Returns:
+            Arbre: Arbre beta-reduit
+        """
+
         nonlocal n_reduccions
         match arbre:
             case Abstraccio(variable, expressio):
@@ -175,8 +343,18 @@ def beta(a: Arbre):
             case Variable(_):
                 return arbre
 
-    # Aplica la substitució d'una beta-reducció
     def substitucio(arbre: Arbre, substitut: Arbre, variable) -> Arbre:
+        """Aplica la substitució d'una beta-reducció
+
+        Args:
+            arbre (Arbre): _description_
+            substitut (Arbre): Arbre substitut
+            variable (_type_): Variable substituida
+
+        Returns:
+            Arbre: Arbre resultant de la substitució
+        """
+
         match arbre:
             case Abstraccio(var, expressio):
                 return Abstraccio(var, substitucio(expressio, substitut, variable))
@@ -200,10 +378,16 @@ def beta(a: Arbre):
     return Variable('Nothing')
 
 
-def imprimeix_arbre(a: Arbre):
+def genera_imatge(a: Arbre):
+    """Funció que genera la imatge output.png de l'arbre
+
+    Args:
+        a (Arbre): Arbre a imprimir
+    """
      
     graph = pydot.Dot('my_graph', graph_type='digraph', bgcolor='white')
 
+    # Visitador per generar la imatge
     def visitador(arbre: Arbre, id):
         match arbre:
             # Els nodes abstracció tindran l'id del seu paràmetre
@@ -236,55 +420,28 @@ def imprimeix_arbre(a: Arbre):
     visitador(a, '')
     graph.write_png('output.png')
 
-# Visitador de la gramàtica
-class TreeVisitor(lcVisitor):
-
-    def visitRoot(self, ctx:lcParser.RootContext):
-        return self.visitChildren(ctx)
-
-    def visitAssignacio(self, ctx:lcParser.AssignacioContext):
-        variable = str(ctx.getChild(0))
-        terme = ctx.getChild(2)
-        macros[variable] = self.visit(terme)
-        return True
-
-    def visitExpressio(self, ctx:lcParser.ExpressioContext):
-        return self.visitChildren(ctx)
-
-    def visitParentesis(self, ctx:lcParser.ParentesisContext):
-        terme = ctx.getChild(1)
-        return self.visit(terme)
-
-    def visitLletra(self, ctx:lcParser.LletraContext):
-        lletra = str(ctx.getChild(0))
-        return Variable(lletra)
-
-    def visitVariable(self, ctx:lcParser.VariableContext):
-        variable = str(ctx.getChild(0))
-        return macros[variable]
-
-    def visitAbstraccio(self, ctx:lcParser.AbstraccioContext):
-        terme = ctx.getChild(ctx.getChildCount() - 1)
-        lletra = ctx.getChild(ctx.getChildCount() - 3)
-        n = Abstraccio(Variable(lletra), self.visit(terme))
-        # traducció abstracció de múltiples paràmetres
-        for i in range(ctx.getChildCount() - 4, 0, -1): n = Abstraccio(Variable(ctx.getChild(i)), n)
-        return n
-
-    def visitAplicacio(self, ctx:lcParser.AplicacioContext):
-        [terme1, terme2] = list(ctx.getChildren())
-        x = str(terme2.getChild(0))
-        # En el cas que es detecti una macro en notació infixa, convertir-la en notació prefixa
-        if (len(x) == 1 and not x.isalpha() and x != '(' and x != '\u005C' and x != 'λ'): 
-            return Aplicacio(self.visit(terme2), self.visit(terme1))
-        return Aplicacio(self.visit(terme1), self.visit(terme2))
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Definició de la comanda de telegram /start
+
+    Args:
+        update (Update): Actualització entrant
+        context (ContextTypes.DEFAULT_TYPE): Context
+    """
+
     name = update.effective_user.first_name
     await update.message.reply_text('Welcome ' + name + '!')
     await update.message.reply_text('Write /help to list the available commands')
 
+
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Definició de la comanda de telegram /help
+
+    Args:
+        update (Update): Actualització entrant
+        context (ContextTypes.DEFAULT_TYPE): Context
+    """
+        
     await update.message.reply_text('/start - shows a welcome message\
                                     \n/help - shows this message\
                                     \n/author - author information \
@@ -307,12 +464,28 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
                                     \n -prefix macro → uppercase letters and numbers, first charachter must be an uppercase letter\
                                     \n - infix macro → symbols such as +,-,*')
 
+
 async def author(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Definició de la comanda de telegram /author
+
+    Args:
+        update (Update): Actualització entrant
+        context (ContextTypes.DEFAULT_TYPE): Context
+    """
+        
     await update.message.reply_text('λ-Calculus Bot\
                                     \nBernat Borràs Civil, 2023\
                                     \nTelegram/GitHub: @BernatBC')
 
+
 async def tracta_expressio(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Tractament de l'expressió introduida per telegram
+
+    Args:
+        update (Update): Actualització entrant
+        context (ContextTypes.DEFAULT_TYPE): Context
+    """
+        
     input_stream = InputStream(update.message.text)
     lexer = lcLexer(input_stream)
     token_stream = CommonTokenStream(lexer)
@@ -327,7 +500,7 @@ async def tracta_expressio(update: Update, context: ContextTypes.DEFAULT_TYPE) -
                 return
             case _:
                 await update.message.reply_text(to_string(arbre))
-                imprimeix_arbre(arbre)
+                genera_imatge(arbre)
                 await update.message.reply_photo(photo=open('output.png', 'rb'))
                 steps.clear()
                 alpha_convertit = alpha(arbre)
@@ -335,13 +508,21 @@ async def tracta_expressio(update: Update, context: ContextTypes.DEFAULT_TYPE) -
                 for s in steps:
                     await update.message.reply_text(s)
                 await update.message.reply_text(to_string(beta_reduit))
-                imprimeix_arbre(beta_reduit)
+                genera_imatge(beta_reduit)
                 await update.message.reply_photo(photo=open('output.png', 'rb'))
     else:
         print(parser.getNumberOfSyntaxErrors(), 'errors de sintaxi.')
         print(tree.toStringTree(recog=parser))
 
+
 async def llista_macros(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Definició de la comanda de telegram /macros
+
+    Args:
+        update (Update): Actualització entrant
+        context (ContextTypes.DEFAULT_TYPE): Context
+    """
+        
     message = ''
     for m in macros:
         message = message + '\n' + m + ' ≡ ' + to_string(macros[m])
@@ -349,12 +530,23 @@ async def llista_macros(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
         message = 'The list of macros is empty\n'
     await update.message.reply_text(message)
 
+
 async def clear(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Definició de la comanda de telegram /clear
+
+    Args:
+        update (Update): Actualització entrant
+        context (ContextTypes.DEFAULT_TYPE): Context
+    """
+        
     macros.clear()
     await update.message.reply_text('Macros have been cleared')
 
-# Funció que permet executar l'intèrpret mitjançant telegram
+
 def run_telegram():
+    """Funció que permet executar l'intèrpret mitjançant telegram
+    """
+
     TOKEN = open('token.txt').read().strip()
     app = ApplicationBuilder().token(TOKEN).build()
 
@@ -367,8 +559,11 @@ def run_telegram():
 
     app.run_polling()
 
-# Funció que permet executar l'intèrpret mitjançant la terminal
+
 def run_terminal():
+    """Funció que permet executar l'intèrpret mitjançant la terminal
+    """
+
     while True:
         try:
             input_stream = InputStream(input('? '))
@@ -398,6 +593,7 @@ def run_terminal():
             break
 
 
+# Inici execució intèrpret. Seleccionar entre execució per terminal o al telegram
 if (len(sys.argv) >= 2 and sys.argv[1] == 'terminal'):
     run_terminal()
 else:
